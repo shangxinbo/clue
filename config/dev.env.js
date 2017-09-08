04c5548cbdce11e1f9fcbae76a65cf9cc92ebdb8
shangxinbo <shangxinbo@geotmt.com>
2017-09-08 09:24:20 +0800

    clue 代理地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   MOCK_SERVER_PORT: '3009',
-  PROXY_HOST: 'https://shangxinbo.github.io/',
+  PROXY_HOST: 'http://clue.geotmt.com/',
   PROXY_PORT: '80'
 })
