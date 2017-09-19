1841dc838e12a5a316208a27e36e95ddef73cd88
shangxinbo <shangxinbo@geotmt.com>
2017-09-19 16:26:19 +0800

    火狐点击不了上传

--- src/components/product/sms/certificate/add.vue
+++ src/components/product/sms/certificate/add.vue
@@ -20,12 +20,12 @@
                                 <label class="name">资质图片</label>
                                 <div class="input-warp upload-warp">
                                     <input class="text" type="text" v-model="filePath" readonly>
-                                    <button class="btn" type="button">
+                                    <a class="btn" style="min-width:50px;">
                                         <span>
                                             <i class="icon upload"></i>上传</span>
                                         <input type="file" style="
                                             height: 30px;
-                                            width: 80px;
+                                            width: 70px;
                                             opacity:0;
                                             cursor:pointer;
                                             position: absolute;
@@ -33,7 +33,7 @@
                                             left: 0;
                                             z-index: 1;
                                         " multiple="false" accept=".png,.jpg,.jpeg,.gif" @change="selectFile($event)" />
-                                    </button>
+                                    </a>
                                     <p v-if="file_error" class="tips error">{{file_error}}</p>
                                 </div>
                             </li>
